42d5cd03a5c073fe07b34cac1411d83c2e1f7ff6
Mor4n <anm0ran@outlook.com>
2025-09-19 21:44:47 -0700

    Estructura inicial de buscador

---
+++ search-service/src/main/java/search_service/ServicioDeBusquedaApplication.java
@@ -0,0 +1,13 @@
+package search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDeBusquedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDeBusquedaApplication.class, args);
+	}
+
+}
---
+++ search-service/src/test/java/search_service/ServicioDeBusquedaApplicationTests.java
@@ -0,0 +1,13 @@
+package search_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioDeBusquedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
